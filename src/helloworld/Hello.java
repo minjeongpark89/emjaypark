8484331bfd8d753098ac3072e2492ce920629765
minjeongpark89 <39620654+minjeongpark89@users.noreply.github.com>
2018-05-25 19:41:45 +0900

    Add files via upload

---
+++ src/helloworld/Hello.java
@@ -0,0 +1,16 @@
+package helloworld;
+/**
+ * 
+ * @author 201802147
+ * 
+ * 표준 출력으로 "Hello World!"를 표시하는 간단한 자바 애플리케이션의 구현이다.
+ *
+ *
+ */
+
+public class Hello {
+	public static void main(String args[]) {
+		System.out.println("Hello Wolrd!"); //문자열 출력
+	}
+
+}
